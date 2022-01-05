482670ba0762e6242bc5dda3536a1c1f15f2850a
Jarno Looij <50831250+jarnolooij@users.noreply.github.com>
2022-01-05 13:51:40 +0100

    update

--- Fibonacci Sequence/Assets/Scripts/planesize.cs
+++ Fibonacci Sequence/Assets/Scripts/planesize.cs
@@ -7,13 +7,13 @@ public class planesize : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.localScale = new Vector3(Camera.main.orthographicSize * 0.29f * Screen.width / Screen.height, Camera.main.orthographicSize * 1.0f, 1f);
+        
     }
 
 
         // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale = new Vector3(Camera.main.orthographicSize * 0.29f * Screen.width / Screen.height, Camera.main.orthographicSize * 1.0f, 1f);
     }
 }
